22db3bc762e56e3f0ee7f52ce4331ab1b0c67a7b
Alexander <protonull@protonmail.com>
2020-06-25 16:06:28 +0100

    Update to use the new version system

--- setup/main.ts
+++ setup/main.ts
@@ -9,9 +9,9 @@ const setupFolder = path(mainFolder, config.folders.setup);
 const cacheFolder = path(setupFolder, config.folders.config);
 const projectFolder = path(setupFolder, config.folders.plugin);
 const templateFolder = path(setupFolder, config.folders.template);
-const pluginLocation = path(projectFolder, "target", config.plugin + ".jar");
-const serverPath = (version) => path(cacheFolder, config.folders.server, version);
-const pluginPath = (version) => path(serverPath(version), "plugins", config.plugin + ".jar");
+const pluginOriginPath = (suffix) => path(projectFolder, "target", `${config.plugin}-${suffix}.jar`);
+const serverPath = (version) => path(cacheFolder, version);
+const pluginPath = (version, suffix) => path(serverPath(version), "plugins", `${config.plugin}-${suffix}.jar`);
 const paperPath = (version) => path(serverPath(version), "paper.jar");
 const generatedPath = (version) => path(serverPath(version), "plugins", config.plugin);
 const reportsPath = (version) => path(reportsFolder, version);
@@ -21,6 +21,16 @@ import { rmdir, mkdir, copy } from "./scripts/utilities";
 import compile from "./scripts/compile-plugin";
 import download from "./scripts/download";
 import run from "./scripts/run-paper";
+
+function parseVersion(raw): string[] {
+     if (typeof raw === "string") {
+          return [raw, "pre1.13"];
+     }
+     if (raw instanceof Array) {
+          return raw.slice(0, 2);
+     }
+     return null;
+}
 
 (async function () {
      // Cleaning up reports
@@ -41,14 +51,20 @@ import run from "./scripts/run-paper";
      }
      // Setup server folder for each version
      console.log("Setting up server environments.");
-     for (const version of config.versions) {
+     for (const confVer of config.versions) {
+          const testVer = parseVersion(confVer);;
+          if (testVer === null) {
+               console.error(chalk.red(`Could not parse version [${confVer}]`));
+               continue;
+          }
+          const [version, plugin] = testVer;
           const serverFolder = serverPath(version);
           console.log(`Ensuring ${version} folder.`);
           mkdir(serverFolder);
           console.log("Applying template.");
           copy(templateFolder, serverFolder, true);
           console.log("Adding plugin.");
-          copy(pluginLocation, pluginPath(version), true);
+          copy(pluginOriginPath(plugin), pluginPath(version, plugin), true);
           console.log("Removing old reports.");
           rmdir(generatedPath(version));
           const paperLocation = paperPath(version);
