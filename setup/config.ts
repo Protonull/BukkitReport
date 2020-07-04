2616567de0061c4e83c4ea18d6456b7e354d7a59
Alexander <protonull@protonmail.com>
2020-07-04 18:18:01 +0100

    Add 1.16 reports

--- setup/config.ts
+++ setup/config.ts
@@ -14,6 +14,7 @@ export default {
     paper: "https://papermc.io/api/v1/paper/%s/latest/download",
 
     versions: [
+        ["1.16.1", "1.16"],
         ["1.15.2", "1.15"],
         ["1.15.1", "1.15"],
         ["1.15", "1.15"],
